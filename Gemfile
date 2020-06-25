f2e9207b31d0d523f35c4632e11ad1759c972a62
link-marie <rijkmarie@gmail.com>
2020-06-25 18:57:38 +0900

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -29,6 +29,12 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'bootstrap-sass', '3.4.1'
 gem 'bcrypt',         '3.1.12'
 
+gem 'faker',          '1.9.1'
+
+gem 'will_paginate',           '3.1.7'
+gem 'bootstrap-will_paginate', '1.0.0'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_092144) do
+ActiveRecord::Schema.define(version: 2020_06_25_081037) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_092144) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
